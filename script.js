21aaa945a1648b38a285a2bac5a28f30b61a761e
Ankit <12kankitsingh@gmail.com>
2023-04-12 00:04:18 +0530

    Done with Intial Structure

---
+++ script.js
@@ -0,0 +1,24 @@
+let words= document.getElementsByClassName("fullpage");
+let body= document.querySelector("body");
+
+setTimeout(function(){
+    words[0].style.transform="translateY(-100%)";
+     words[0].style.borderRadius="0 0 50% 50%"
+     words[0].style.scale= "1.1"
+    //  body[0].classlist;
+    console.log(body.classList);
+    body.classList.remove("intialHeight");
+    //  bodyClasslist.remove("intialHeight")
+},1600)
+
+let text = document.getElementsByClassName("candid");
+// let textParallax=document.querySelector(".candid");
+console.log(text);
+
+        window.addEventListener("scroll",function(){
+            
+            let value = window.scrollY;
+            if(value<=536){
+            text[0].style.marginLeft = value*1.1 + 'px';}
+            
+        })
